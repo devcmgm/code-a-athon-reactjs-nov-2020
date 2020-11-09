d9755ca7aaa402115e1736695fae1187d66895d0
zach lewis <zlewis@clarityinnovates.com>
2020-11-09 11:20:35 -0600

    initial code #6

--- in-memory-datastore/src/api/backendRequestProcessor.js
+++ in-memory-datastore/src/api/backendRequestProcessor.js
@@ -1,6 +1,24 @@
+var PouchDB = require('pouchdb');
+var db = new PouchDB('my_database');
+
+function showTodos() {
+  db.allDocs({include_docs: true, descending: true}, function(err, doc) {
+   return doc.rows;
+  });
+}
+
 module.exports = (req, res) => {
+  db.put(res.json, function callback(err, result) {
+    if (!err) {
+      console.log('Successfully posted a todo!');
+    }
+  });
+  var data =  db.allDocs({include_docs: true, descending: true}, function(err, doc) {
+    return doc.rows;
+  });
+
   res.json({
-    body: req.body,
+    body: req.body + data,
     query: req.query,
     cookies: req.cookies,
   })
