76307c8c76b2b2927a68779967317ed794500ddb
zach lewis <zlewis@clarityinnovates.com>
2020-11-09 11:27:49 -0600

    initial code #9

--- in-memory-datastore/src/api/inMemoryDBRequestProcessor.js
+++ in-memory-datastore/src/api/inMemoryDBRequestProcessor.js
@@ -1,24 +1,18 @@
 var PouchDB = require('pouchdb');
 var db = new PouchDB('my_database');
 
-function showTodos() {
-  db.allDocs({include_docs: true, descending: true}, function(err, doc) {
-   return doc.rows;
-  });
-}
 
 module.exports = (req, res) => {
-  db.put(res.json, function callback(err, result) {
+  db.put(JSON.stringify(req.json), function callback(err, result) {
     if (!err) {
       console.log('Successfully posted a todo!');
+    } else {
+      console.log('error:' + err)
     }
-  });
-  var data =  db.allDocs({include_docs: true, descending: true}, function(err, doc) {
-    return doc.rows;
   });
 
   res.json({
-    body: req.body + data,
+    body: req.body,
     query: req.query,
     cookies: req.cookies,
   })
