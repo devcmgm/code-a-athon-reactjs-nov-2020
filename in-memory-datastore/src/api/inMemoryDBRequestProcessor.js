19aa0892cf9f7d4f4b6a90d02c4bb592ea26dc7a
zach lewis <zlewis@clarityinnovates.com>
2020-11-09 11:32:01 -0600

    initial code #100

--- in-memory-datastore/src/api/inMemoryDBRequestProcessor.js
+++ in-memory-datastore/src/api/inMemoryDBRequestProcessor.js
@@ -1,15 +1,7 @@
-var PouchDB = require('pouchdb');
-var db = new PouchDB('my_database');
-
 
 module.exports = (req, res) => {
-  db.put(JSON.stringify(req.json), function callback(err, result) {
-    if (!err) {
-      console.log('Successfully posted a todo!');
-    } else {
-      console.log('error:' + err)
-    }
-  });
+  const PouchDB = require('pouchdb');
+  const db = new PouchDB('my_database');
 
   res.json({
     body: req.body,
